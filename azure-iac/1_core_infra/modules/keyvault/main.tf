28500d44c3d976ad3bc07b89e2b5d9eb51eafa10
janvi <janvi@bishopal.com>
2025-09-15 13:05:33 +0530

    aces provided

--- azure-iac/1_core_infra/modules/keyvault/main.tf
+++ azure-iac/1_core_infra/modules/keyvault/main.tf
@@ -25,6 +25,7 @@ resource "azurerm_key_vault" "vault" {
 }
 
 resource "azurerm_key_vault_access_policy" "default_vault_access_policy" {
+  depends_on = [ azurerm_key_vault.vault ]
   key_vault_id = azurerm_key_vault.vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
